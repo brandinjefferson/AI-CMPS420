8ca1eae3ec8fba8e76fa359f93f16926dc02551e
Brandin Jefferson <brandinjefferson93@gmail.com>
2015-03-12 23:46:39 -0500

    Seems to be working

--- GeneticOperator.py
+++ GeneticOperator.py
@@ -1,5 +1,6 @@
 import random
 import string
+import math
 
 alphabet = string.letters
 
@@ -11,15 +12,18 @@ class Chromosome:
 	def evalFitness(self,expression,vars):
 		bit_str = ""
 		activeNot = False
-		for x in expression:
+		for i in range(0,len(expression)):
+			x = expression[i]
 			if x not in (alphabet + "!"):
-				if x != "*":
-					bit_str += x
+				if x == "*" or i == len(expression)-1:
 					if x == ")":
-						result = eval(bit_str)
-						if result > 0:
-							self.fitness+=1
-						bit_str=""
+						bit_str += x
+					result = eval(bit_str)
+					if result > 0:
+						self.fitness += 1
+					bit_str = ""
+				else:
+					bit_str += x
 			else:
 				if x == "!":
 					activeNot = True
@@ -31,14 +35,19 @@ class Chromosome:
 							bit_str += "0"
 						activeNot = False
 					else:
-						bit_str += self.bits[vars.index(x)]
+						bit_str += str(self.bits[vars.index(x)])
+						print "bit_str = %s" %str(self.bits[vars.index(x)])
+						if i == len(expression)-1:
+							result = eval(bit_str)
+							if result > 0:
+								self.fitness += 1
 						
 	def setBits(self,numVars):
 		for x in range(0,numVars):
 			self.bits.append(random.randint(0,1))
 
 	def mutate(self):
-		index = random.randint(0,len(self.bits))
+		index = random.randint(0,len(self.bits)-1)
 		if self.bits[index] == 0:
 			self.bits[index] = 1
 		else:
@@ -53,13 +62,18 @@ def geneticOperations(numVars,c1,c2):
 	max = random.randint(1,numVars)
 	child1 = c1
 	child2 = c2
+	child1.fitness = 0
+	child2.fitness = 0
 	while max <= numVars:
 		part1 = c1.bits[min:max]
 		part2 = c2.bits[min:max]
 		child1.bits[min:max] = part2
 		child2.bits[min:max] = part1
 		min = max+1
+		if min >= max:
+			break
 		max = random.randint(min,numVars)
+		
 	mutate = random.randint(0,1)
 	if mutate == 1:
 		child1.mutate()
@@ -70,7 +84,8 @@ def geneticOperations(numVars,c1,c2):
 		
 def evaluateChromosomeFitness(c,d,vars):
 	if c.fitness == d:
-		print "%s = %s" %(str(vars).strip("[]"),str(c).strip("[]"))
+		print "Fitness Level = %d" %c.fitness 
+		print "%s = %s" %(str(vars).strip("[]"),str(c.bits).strip("[]"))
 		return True
 	else:
 		return False
@@ -79,15 +94,17 @@ def findFitnessRanges(population):
 	total = 0
 	tuples = []
 	min = 0
+	ratio = 0
 	for x in range(0,len(population)):
 		total += float(population[x].fitness)
 	for x in range(0,len(population)):
-		ratio = int((population[x].fitness/total) * 100)
-		tuples.append((min,min+ratio))
+		if total > 0:
+			ratio = int((population[x].fitness/total) * 100)
+		tuples.append((min,min+ratio))	
 		min = min+ratio+1
 	return tuples
 
-def getRandomNumbers():
+def getRandomNumbers(ranges):
 	r1 = random.randint(0,100)
 	r2 = random.randint(0,100)
 	index1 = 0
@@ -95,12 +112,14 @@ def getRandomNumbers():
 	for x in range(0,len(ranges)):	
 		if r1 in range(ranges[x][0],ranges[x][1]):
 			index1 = x
+			#print "Index1: %d" %index1
 		if r2 in range(ranges[x][0],ranges[x][1]):
 			index2 = x
+			#print "Index2: %d" %index2
 		if index1 == index2:
 			r2 = random.randint(0,100)
 			x = 0
-	array = [index1,index2]
+	return [index1,index2]
 	
 continue_prog = "y"
 
@@ -110,22 +129,23 @@ print "to true."
 print "+ := OR"
 print "* := AND"
 print "! := NOT"
+print "Example: (A+B)*(!B+C+!D)*(D+!E)"
 
-expr = raw_input("Enter Expression: ")
-
-while continue_prog == "y" or continue_prog == "yes"
+while continue_prog.lower() == "y" or continue_prog == "yes":
+	expr = raw_input("Enter Expression: ")
 	#Parse string and get unique variables
-	disjuncts = 0		#The total number of disjuncts
+	disjuncts = 1		#The total number of disjuncts
 	var_list = []		#A list of the vars in the expression
 	for x in expr:
 		#in and not in check single character strings to see if they're in 
 		#the list
-		if x in alphabet and x not in expr_list:
+		if x in alphabet and x not in var_list:
 			var_list.append(x)
-		elif x == ")":
+		elif x == "*":
 			disjuncts += 1
 	numVars = len(var_list)
-	
+	#print "Disjuncts = %d" %disjuncts
+	var_list.sort()
 	#Generate initial population (create random bits for each variable)
 	population = []
 	popsize = int(math.log(numVars,2))
@@ -137,8 +157,7 @@ while continue_prog == "y" or continue_prog == "yes"
 		aChrom = Chromosome()
 		aChrom.setBits(numVars)
 		population.append(aChrom)
-	
-	
+	print "Originals: %s, %s" %(str(population[0].bits),str(population[1].bits))
 	#Perform genetic algorithm until problem is solved
 	solved = False
 	time = 0
@@ -146,6 +165,7 @@ while continue_prog == "y" or continue_prog == "yes"
 		#Evaluate fitness of each member of P(t)
 		for x in range(0,len(population)):
 			population[x].evalFitness(expr,var_list)
+			print population[x].fitness
 			if evaluateChromosomeFitness(population[x],disjuncts,var_list):
 				solved = True
 				break
@@ -158,23 +178,26 @@ while continue_prog == "y" or continue_prog == "yes"
 		indexes = []
 		newpopulation = []
 		if popsize >= 2:
-			list = getRandomNumbers()
+			list = getRandomNumbers(ranges)
 			indexes.append(list[0])
-			indexes.append[list[1]]
+			indexes.append(list[1])
 			#Perform genetic operations 
 			newpopulation = geneticOperations(numVars,population[indexes[0]],population[indexes[1]])
 		if popsize == 4:
-			list = getRandomNumbers()
+			list = getRandomNumbers(ranges)
 			indexes.append(list[0])
-			indexes.append[list[1]]
+			indexes.append(list[1])
 			additionalpop = geneticOperations(numVars,population[indexes[2]],population[indexes[3]])
 			newpopulation.append(additionalpop[0])
 			newpopulation.append(additionalpop[1])
 		#Replace old population with new population
-		delete population
 		population = newpopulation
 		time += 1
-		print time
+		print "time = %d" %time
+		for x in range(0,len(population)):
+			print population[x].bits
+		
+	continue_prog = raw_input("Go again (yes/no)? ")
 			
 	
 	
